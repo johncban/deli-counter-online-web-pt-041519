770e5ed843dcf6b048347ceff6e9ef2d0cb2fc67
johncban <johncban@gmail.com>
2019-04-21 22:25:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty? == true
     puts "The line is currently empty."
   else
     message = "The line is currently:"
@@ -20,6 +20,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving()
+def now_serving(katz_deli)
+  if katz_deli.empty?
 
 end
