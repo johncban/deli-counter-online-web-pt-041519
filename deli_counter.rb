52d9f2465e36d7206089129a2d630d00fc5889df
johncban <johncban@gmail.com>
2019-04-21 22:07:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,17 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    num_line = 1
-    message = "The line is currently:"
-    # Source: https://stackoverflow.com/questions/20258086/difference-between-each-with-index-and-each-with-index-in-ruby
-    katz_deli.each_with_index do |customer, num_line|
-      message.push "#{num_line}. #{customer}"
-      num_line += 1
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
     end
-    puts "#{message}"
-  end
+    puts "The line is currently: #{line_positions.join(" ")}"
+end
 end
 
 def take_a_number(katz_deli, name)
