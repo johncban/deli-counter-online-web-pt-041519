b99044a34ec0e99268a472e43ced465c204de2d5
johncban <johncban@gmail.com>
2019-04-21 22:28:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.empty? == false
-    puts "Currently serving #{katz_deli.shift}"
+  else
+    puts "Currently serving #{katz_deli.first}"
   end
 end
