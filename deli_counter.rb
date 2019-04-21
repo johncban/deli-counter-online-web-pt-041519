08b7f84ccc03e189e0533966cf0d2a82c349d63b
johncban <johncban@gmail.com>
2019-04-21 20:52:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(customer)
   if customer.length == 0
-    puts "The line is currently empty"
+    puts "The line is empty"
   else
     message = "The line is currently:"
     # Source: https://stackoverflow.com/questions/20258086/difference-between-each-with-index-and-each-with-index-in-ruby
