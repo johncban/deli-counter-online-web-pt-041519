8708a0d50a26c6c42022501d7251a53a278c35ba
johncban <johncban@gmail.com>
2019-04-21 21:53:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.prepend name
+  katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
